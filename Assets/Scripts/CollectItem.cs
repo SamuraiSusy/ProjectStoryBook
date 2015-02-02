e864c7545475fe0e8da1c51690bda74f4d269166
Susanna Nurmi <susyy-@hotmail.com>
2015-02-02 11:35:46 +0200

    identyfies item
    can destroy item when pressing space

---
+++ Assets/Scripts/CollectItem.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectItem : MonoBehaviour
+{
+    private bool foundItem;
+
+	// Use this for initialization
+	void Start ()
+    {
+        foundItem = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Collect();
+	}
+
+    void Collect()
+    {
+        if(foundItem)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Destroy(GameObject.FindWithTag("Collectable"));
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Collectable")
+        {
+            foundItem = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Collectable")
+        {
+            foundItem = false;
+        }
+    }
+}
---
+++ Assets/Scripts/tarvitaak/Potion.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Potion : Item {
+
+    private string potionName;
+    private string potionDescription;
+    private int potionID;
+
+    public enum PotionTypes
+    {
+        HP,
+        MANA
+    }
+    private PotionTypes potionType;
+
+    public string PotionName
+    {
+        get { return potionName; }
+        set { potionName = value; }
+    }
+    public string PotionDescription
+    {
+        get { return potionDescription; }
+        set { potionDescription = value; }
+    }
+    public int PotionID
+    {
+        get { return potionID; }
+        set { potionID = value; }
+    }
+    public PotionTypes PotionType
+    {
+        get { return potionType; }
+        set { potionType = value; }
+    }
+}
