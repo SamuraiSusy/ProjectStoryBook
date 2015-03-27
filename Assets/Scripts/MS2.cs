32dc242c4332adcac404b66146454bef5dba4bb8
Susanna Nurmi <susyy-@hotmail.com>
2015-03-27 16:04:28 +0200

    new textbox
    testing the new system, still in development

---
+++ Assets/Scripts/MS2.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// avoid putting anything useless to ongui, it messes up stuff..
+public class MS2 : MonoBehaviour
+{
+    public GUISkin skin;
+    public bool showBox;
+    private MessagePlaceholders messages;
+    private ChangeBoxContent changeContent;
+    private ChoiseButtons answer;
+    public string currentMessage;
+    public int count, count2;
+    public string[] curTextBoxContent;
+
+    // Use this for initialization
+    private void Start()
+    {
+        showBox = false;
+        messages = GetComponent<MessagePlaceholders>();
+        changeContent = GameObject.FindGameObjectWithTag("Player").GetComponent<ChangeBoxContent>();
+        answer = GetComponent<ChoiseButtons>();
+        currentMessage = "";
+        count = 0;
+        count2 = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (showBox && Input.GetButtonUp("Collect"))
+        {
+            WriteMessages();
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (showBox)
+        {
+            DrawBox();
+        }
+    }
+
+    private void DrawBox()
+    {
+        GUI.skin = skin;
+        Rect mssbox = new Rect((Screen.width / 2) - (Screen.width / 2.9f), Screen.height / 1.4f, Screen.width / 1.4f, Screen.height / 3.7f);
+        GUI.TextField(mssbox, currentMessage, skin.GetStyle("Message"));
+    }
+
+    // draws the messages to the text box
+    private void WriteMessages()
+    {
+
+        for (int i = 0; i < messages.dialogues.Length; i++)
+        {
+            if (curTextBoxContent.GetHashCode() == messages.dialogues[i].GetHashCode())
+            {
+                if (count < messages.dialogues[i].Length)
+                {
+                    currentMessage = PrintMessages(messages.dialogues[i], count);
+                    count++;
+                    if (count == messages.dialogues[i].Length - 1)
+                    {
+                        if (messages.dialogues[i][messages.dialogues[i].Length - 1] == "$")
+                        {
+                            showBox1 = false;
+                            curTextBoxContent = messages.choises[i];
+                            answer.showButtons = true;
+                        }
+                    }
+
+                    else if (curTextBoxContent.GetHashCode() == messages.dialogues[i].GetHashCode() && count >= messages.dialogues[i].Length)
+                    {
+                        showBox1 = false;
+                        showMessages = false;
+                        changeContent.triggers[i] = false;
+                        currentMessage = "";
+                        count = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    // generates the message shown in the message box
+    private string PrintMessages(string[] messageArray, int messageID)
+    {
+        currentMessage = "";
+        currentMessage += messageArray[messageID];
+
+        return currentMessage;
+    }
+}
---
+++ Assets/Scripts/testaus.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testaus : MonoBehaviour
+{
+    private bool show;
+    private int arrayID, count;
+    private string teksti;
+    private string[][] strings;
+    private string[] strings1;
+    private string[] strings2;
+    // Use this for initialization
+    void Start()
+    {
+        arrayID = 0;
+        count = 0;
+        teksti = "";
+
+        strings1 = new string[4];
+        strings1[0] = "moi";
+        strings1[1] = "terve";
+        strings1[2] = "ihqu";
+        strings1[3] = "ihana";
+
+        strings2 = new string[4];
+        strings1[0] = "moi2";
+        strings1[1] = "terve2";
+        strings1[2] = "ihqu2";
+        strings1[3] = "ihana2";
+
+        strings = new string[2][];
+        strings[0] = strings1;
+        strings[1] = strings2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            show = !show;
+
+        if (show && Input.GetKeyUp(KeyCode.Space))
+        {
+            func2();
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (show)
+            func1();
+    }
+
+    private void func1()
+    {
+        GUI.TextField(new Rect(10, 10, 100, 100), teksti);
+    }
+
+    private void func2()
+    {
+        Debug.Log(count);
+        if (count < strings[arrayID].Length)
+        {
+            teksti = strings[arrayID][count];
+            count++;
+        }
+        else
+        {
+            show = false;
+            count = 0;
+            arrayID = 0;
+            teksti = "";
+        }
+    }
+
+    //private void OnTriggerEnter2D(Collider2D col)
+    //{
+    //    if (col.gameObject.tag == "test1")
+    //    {
+    //        arrayID = 0;
+    //    }
+
+    //    if (col.gameObject.tag == "test2")
+    //    {
+    //        arrayID = 1;
+    //    }
+    //}
+}
