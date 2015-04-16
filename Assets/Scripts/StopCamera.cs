c75916f257c1c4995b882ca39a7d0d5c88aaa7a4
Susanna Nurmi <susyy-@hotmail.com>
2015-04-17 00:08:34 +0300

    camera~~

--- Assets/Scripts/StopCamera.cs
+++ Assets/Scripts/StopCamera.cs
@@ -3,40 +3,39 @@ using System.Collections;
 
 public class StopCamera : MonoBehaviour
 {
-    public Camera cameralvl1left, cameralvl1rigth;
-    private Vector3 posLvl1Left, posLvl1Right;
+    private Camera kamera;
+    private PlayerControl player;
+    private Vector3 playerPos;
 
 	// Use this for initialization
 	void Start ()
     {
-        cameralvl1left.enabled = false;
-        posLvl1Left = cameralvl1left.ScreenToViewportPoint(new Vector3(-20.83f, -0.08f, -10f));
+        kamera = Camera.main;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        //if(camera.enabled)
-        //{
-        //    camera.transform.position = posLvl1Left;
-        //}
+        playerPos = player.transform.position;
+        StopLvl1();
 	}
 
-    private void OnTriggerEnter2D(Collider2D col)
+    private void StopLvl1()
     {
-        if(col.gameObject.tag == "Player")
+        if(playerPos.y < kamera.WorldToViewportPoint(new Vector3(0, -1.4f)).y &&
+            playerPos.y > kamera.WorldToViewportPoint(new Vector3(0, 4.5f)).y)
         {
-            Debug.Log(col.gameObject.tag);
-            //Camera.main.enabled = false;
-            cameralvl1left.enabled = true;
-        }
-    }
-    private void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.gameObject.tag == "Player")
-        {
-            cameralvl1left.enabled = false;
-            //Camera.main.enabled = true;
+            // left
+            if(playerPos.x <= kamera.WorldToViewportPoint(new Vector3(-8.8f, 0)).x)
+            {
+                kamera.transform.position = new Vector3(-8.75f, -1.22f, -10f);
+            }
+            // right
+            if (playerPos.x <= kamera.WorldToViewportPoint(new Vector3(-10.4f, 0)).x)
+            {
+
+            }
         }
     }
 }
