29f74221034af3643a13325b6c039be4227bab70
Susanna Nurmi <susyy-@hotmail.com>
2015-07-05 05:05:11 +0300

    move camera
    new, improved camera system, works with colliders, working only one
    time, then stops working

---
+++ Assets/Scripts/MoveCameras.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// ota käyttöön toinen kamera, kun seinä tulee vastaan
+// collideri, joka triggeröi toisen kameran, bool on/off
+
+
+public class MoveCameras : MonoBehaviour
+{
+
+    public Camera movingCamera;
+    private Camera mainCam;
+    private bool isItEnabled;
+    private bool enabledOnce, twice;
+
+    public float camX, camY;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mainCam = Camera.main;
+        isItEnabled = false;
+        enabledOnce = false;
+        twice = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (isItEnabled)
+            MoveOtherCamera();
+	}
+
+    private void MoveOtherCamera()
+    {
+        movingCamera.transform.position = new Vector3(camX, camY, -10);
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player" && !isItEnabled && !enabledOnce)
+        {
+            // enabloi toinen kamera, kävelee seinää kohti
+            movingCamera.enabled = true;
+            mainCam.enabled = false;
+
+            isItEnabled = true;
+            enabledOnce = true;
+        }
+
+        if (col.gameObject.tag == "Player" && isItEnabled && !enabledOnce)
+        {
+            // disabloi toinen kamera, main käyttöön, kävelee pois seinältä
+            mainCam.enabled = true;
+            movingCamera.enabled = false;
+
+            isItEnabled = false;
+            enabledOnce = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player" && isItEnabled)
+        {
+            enabledOnce = false;
+        }
+    }
+}
