baa73c5f4f4c81bbc37c624b9d50efaeb5ae0d23
Susanna Nurmi <susyy-@hotmail.com>
2015-07-06 13:26:08 +0300

    camera stops to walls
    All the stops are now included

---
+++ Assets/Scripts/ResetMovingCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetMovingCamera : MonoBehaviour
+{
+    public Camera movingCam;
+
+	// Use this for initialization
+	void Start ()
+    {
+        movingCam.transform.position = Camera.main.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
